e9d62baeb69c7f7d5868c61fba6e52a265fd7471
Olga <o.nikolaichenko@ya.ru>
2024-07-30 15:36:08 +0200

    project 26

---
+++ 26-react-router/src/components/About.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+const About = () => {
+  return (<>
+    <h1>About</h1>
+   </>
+  )
+}
+
+export default About
---
+++ 26-react-router/src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Menu = () => {
+  return (
+    <nav>
+      {/* свойство end - изменять стили для этой ссылки, только если мы находимся непосредственно на ней*/}
+      <NavLink style={ ( { isActive } ) => isActive ? {color:'orange', textDecoration: 'none'} : {} } to='.' end>Home</NavLink>
+      <NavLink className={ ( { isActive } ) => isActive ? 'activeLink' : 'link'} to='about'>About</NavLink>
+      <NavLink to='contacts'>Contacts</NavLink>
+    </nav>
+  )
+}
+
+export default Menu
